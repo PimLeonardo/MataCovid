42737535ad70ad34f08b90f3275957f998fb8b96
Leonardo Pimentel <49254413+PimLeonardo@users.noreply.github.com>
2021-02-22 14:46:50 -0300

    Add files via upload

--- Mata Covid/jogo.js
+++ Mata Covid/jogo.js
@@ -26,10 +26,32 @@ function tamanhoRandom() {
 	}
 }
 
+//logica para as vidas 
+var vidas = 1
+var tempo = 10
+var relogio = setInterval(function() {
+	tempo -= 1
+	if (tempo < 0) {
+		clearInterval(relogio)
+		clearInterval(criaVirus)
+		alert('Vitoria')
+	} else {
+		document.getElementById('tempo').innerHTML = tempo
+	}
+}, 1000)
+
 function posicaoVirus() {
-	//remover virus anterior 
+	//perder vida se o elemento nao for removido por click
 	if (document.getElementById('virus')) {
 		document.getElementById('virus').remove()
+
+		//console.log(vidas)
+		if (vidas > 3) {
+			window.location.href = 'game_over.html'
+		} else {
+			document.getElementById('vida' + vidas).src = "img/coracao_vazio.png"
+			vidas++
+		}
 	}
 	
 	var posicaoX = Math.floor(Math.random() * largura) - 80
@@ -48,6 +70,9 @@ function posicaoVirus() {
 	virus.style.left = posicaoX + 'px'
 	virus.style.position = 'absolute'
 	virus.id = 'virus'
+	virus.onclick = function() {
+		this.remove()
+	}
 
 	document.body.appendChild(virus)
 }
